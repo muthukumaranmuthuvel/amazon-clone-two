336aa0517a578bf24f16ced0fd00cd581df38e84
muthukumaranmuthuvel <mmkumaran93@hotmail.com>
2021-09-22 17:07:02 +0530

    git commit -m "db connected"

--- firebase.js
+++ firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+import {getFirestore} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyCFNMjx9XxMQ84fh8eBRf9z5facg0lZeTs",
   authDomain: "clone-two-10bdb.firebaseapp.com",
@@ -6,10 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "889191989328",
   appId: "1:889191989328:web:d9585bc0544618355a9155",
   measurementId: "G-434W2M2DF3"
-};
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore();
-console.log(db);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  export const db = getFirestore(app);
+  
--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-db.collection("users").get().then((querySnapshot) => {
+import {db} from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+
+async function  getItems(){
+    const querySnapshot = await getDocs(collection(db, "items"));
+    let items=[];
     querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
+        items.push({
+            id:doc.id,
+            image:doc.data().image,
+            name:doc.data().name,
+            make:doc.data().make,
+            rating:doc.data().rating,
+            price:doc.data().price
+        })
+        
+        generateItems(items);
     });
-});
+}
+
+function generateItems(items){
+    let itemsHTML ="";
+    items.forEach((item)=>{
+        itemsHTML +=`
+        <div class="main-product ml-4 ">
+            <div class="product-image w-48 h-52 bg-white rounded-lg p-4">
+                <img class="w-full h-full object-contain" src="${item.image}" >
+            </div>
+            <div class="product-name text-gray-700 font-bold mt-2 text-sm">
+            ${item.name}
+            </div>
+            <div class="product-make text-green-700 font-bold">
+            ${item.make}
+            </div>
+            <div class="product-rating text-yellow-300 my-1">
+            ⭐⭐⭐⭐⭐ ${item.rating}
+            </div>
+            <div class="product-price font-bold text-gray-700">
+            $${item.price}
+            </div>
+            <div class="add-to-cart h-8 w-28 bg-yellow-500 flex items-center justify-center text-white rounded text-md cursor-pointer hover:bg-yellow-600">
+            Add to cart
+            </div>
+        </div>
+
+        `
+
+    })
+    document.querySelector(".main-section-deals").innerHTML = itemsHTML;
+}
+getItems();
